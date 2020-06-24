67ea8cb7ef9d772bd71c49b0a63e30999305fdb1
ed-commits <63356087+ed-commits@users.noreply.github.com>
2020-06-24 15:50:32 +0100

    listing countries

--- src/components/CountriesList.vue
+++ src/components/CountriesList.vue
@@ -1,9 +1,21 @@
 <template>
-  <div>This is the countries list.</div>
+  <div>
+    This is the countries list.
+    <ul>
+      <li v-for="(item, index) in items" :key="index" :item="item">{{ item.name }}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "countries-list",
+  data() {
+    return {
+      items: [{ name: "Afghanistan" }, { name: "Zimbabwe" }]
+    };
+  }
+};
 </script>
 
 <style>
