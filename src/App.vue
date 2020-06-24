bcd58dd9846601e885ca7b96b028c5ffcf1939d2
ed-commits <63356087+ed-commits@users.noreply.github.com>
2020-06-24 15:38:24 +0100

    initial setup

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CountriesList />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CountriesList from "./components/CountriesList.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    CountriesList
   }
-}
+};
 </script>
 
 <style>
